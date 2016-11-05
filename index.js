989f91ab716d905ae06c44d2f83fcbf53733bab7
jvalli <jeanettv@surveymonkey.com>
2016-11-05 15:17:04 -0700

    added user team and opt team displays on page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//User constructors
+//User constructora
 
-function User (firstName, lastName, dob, gender, weight, height, city, state, indPoints, team, teamPts, userBadge, avatar) {
+function User (firstName, lastName, dob, gender, weight, height, city, state, indPoints, team, userBadge, avatar) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.dob = dob;
@@ -11,42 +11,96 @@ function User (firstName, lastName, dob, gender, weight, height, city, state, in
   this.state = state;
   this.indPoints = indPoints;
   this.team = team;
-  this.teamPts = teamPts;
-  this.userBadge = userBadge;
+  this.userBadge = userBadge
   this.avatar = avatar;  
 };
 
 
-// Badges using icons
+//Activity constructor 
+function Activity(name, points, time, icon){
+  this.name = name;
+  this.points = points;
+  this.time = time;
+  this.icon = icon;
+}
+
+
+// Badge constructors
 function Badge (name, icon, bonusPts) {
   this.name = name;
   this.icon = path + icon;
   this.bonusPts = bonusPts;
 };
 
-var path = "styles/img/";
+//Team constructor
+function Team (name, members, teamPts, opponent) {
+  this.name = name;
+  this.members = members;
+  this.teamPts = teamPts;
+  this.opponent = opponent;
+};
 
-
-//Badge constructors
-
-var mvp = new Badge("MVP", "shield.png", 10);
+var path = "./styles/img/";
+var mvp = new Badge("MVP", "badge.png", 10);
 var threeActivities = new Badge("3 Activities", "bronze.png", 5);
 var fiveActivities = new Badge("5 Activities", "silver.png", 5);
 var tenActivities = new Badge("10 Activities", "gold.png", 10);
 var joinTeam = new Badge("Joined a Team", "shield.png", 5);
 
-//temp global badge variable
-var userBadge =[threeActivities];
+//mock data
+//users
+var Jen = new User("Jen","Ferro","","Female","","","Portland","OR",100,"Oscar",[joinTeam,mvp,threeActivities,fiveActivities], "");
+var James = new User("James","Smith","","Male","","","Portland","OR",110,"Oscar",[joinTeam,mvp,threeActivities,fiveActivities],"");
+var Jeanette = new User("Jeanette","Valli","","Female","","","Portland","OR",60,"bigBird",[joinTeam,threeActivities],"");
+var Brigette = new User("Brigette","Eckert","","Female","","","Portland","OR",180,"bigBird",[joinTeam,mvp,threeActivities,fiveActivities,tenActivities],"");
+//teams
+var bigBird = new Team("Big Bird", [Brigette,Jeanette],240,Oscar);
+var Oscar = new Team("Oscar", [James,Jen],210,bigBird);
+//activites 
 
 
-//display badges. Adds badge to userBadges
+
+ //Adds badge to userBadges when user earns new badge
 function earnBadge(badgeEarned){
   userBadge.push(badgeEarned);
-  console.log(userBadge);
-  for (i=0; i < userBadge.length; i++) {
-    console.log(userBadge[i].icon);
-  $('div.badges').append("<img src=" + userBadge[i].icon + "></img> <p>" + userBadge[i].name + "</p>");
-  }
 };
 
-earnBadge(mvp);
+//earnBadge(mvp);
+
+//Display individual data 
+
+function displayUser(currentUser){
+  $('div.solopoints').append("<p>My Points" + currentUser.indPoints + "/ 500 </p> <p> Progress towards $25 Nike Gift Card");
+  for (i=0; i < currentUser.userBadge.length; i++) {
+      console.log(currentUser.userBadge[i].icon);
+    $('div.badges').append("<img src=" + currentUser.userBadge[i].icon + "></img> <p>" + currentUser.userBadge[i].name + "</p>");
+    }
+
+};
+
+//display team dat
+function displayTeam(userTeam){
+  console.log(userTeam);
+  $('div.teamPts').append("<h3> My Team: " + userTeam.name + "</h3> <p>Team Points" + userTeam.teamPts + "</p>");
+  for (i=0; i < userTeam.members.length; i++) {
+    $('div.teamMembers').append(userTeam.members[i].name);
+    }
+};
+
+// function displayOpt(optTeam){
+//   console.log(optTeam);
+//   $('div.optPts').append("<h3> Rival Team: " + optTeam.name + "</h3> <p>Team Points" + optTeam.teamPts + "</p>");
+//   for (i=0; i < optTeam.members.length; i++) {
+//     $('div.optMembers').append(optTeam.members[i].name);
+//     }
+// };
+
+displayUser(Jen);
+displayTeam(Oscar);
+// displayOpt(Big Bird);
+
+//add activity 
+
+
+//set up alogirthm to determine points per time. low intensity .5pt per min, high intensity = 1 pt per mins
+
